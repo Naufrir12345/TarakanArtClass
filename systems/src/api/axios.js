3047a724bb852d577cc6c24dc9a5cff305277525
Naufrir12345 <kurniawan.naufrir@gmail.com>
2026-07-19 01:42:54 +0700

    Testing Deploy 3

--- systems/src/api/axios.js
+++ systems/src/api/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.VITE_API_URL || import.meta.env.VITE_API_URL
 });
 
 // Interceptor Request
