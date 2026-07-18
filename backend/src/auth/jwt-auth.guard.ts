727070e85b6535b2b8754b29816a917ecaab8108
Naufrir12345 <kurniawan.naufrir@gmail.com>
2026-07-19 03:17:55 +0700

    Production 1.1

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,13 +1,5 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-// @Injectable()
-// export class JwtAuthGuard extends AuthGuard('jwt') { }
-
-
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-        return true; // <--- Cukup tambahkan baris ini agar semua Guard selalu meloloskan akses
-    }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
