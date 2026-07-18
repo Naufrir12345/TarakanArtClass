a3cb17f08c50b2267788edd6d0e2078cd2f739f2
Naufrir12345 <kurniawan.naufrir@gmail.com>
2026-07-19 01:46:44 +0700

    Testing Deploy 4

--- systems/src/api/axios.js
+++ systems/src/api/axios.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-// });
-
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL || import.meta.env.VITE_API_URL
+  // Tulis langsung di sini, jangan ambil dari mana-mana dulu
+  baseURL: 'https://tarakanartclass-production.up.railway.app'
 });
 
 // Interceptor Request
@@ -17,13 +14,9 @@ api.interceptors.request.use((config) => {
   return config;
 }, (error) => Promise.reject(error));
 
-// Interceptor Response (DI SINI KEMUNGKINAN ERROR-NYA)
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
-
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
