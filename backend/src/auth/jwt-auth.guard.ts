6a9f86c207fc98733d2ff1b8f4916340c63788a9
Naufrir12345 <kurniawan.naufrir@gmail.com>
2026-07-19 00:27:53 +0700

    Disable Auth Guard sementara untuk testing

--- backend/src/auth/jwt-auth.guard.ts
+++ backend/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+// @Injectable()
+// export class JwtAuthGuard extends AuthGuard('jwt') { }
+
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class JwtAuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        return true; // <--- Cukup tambahkan baris ini agar semua Guard selalu meloloskan akses
+    }
+}
