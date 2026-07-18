ce2e8354d2ea2c159f094df6e0d590742472bb74
Naufrir12345 <kurniawan.naufrir@gmail.com>
2026-07-19 00:56:46 +0700

    Disable Auth Guard sementara untuk testing

--- systems/src/api/axios.js
+++ systems/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+// });
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://tarakanartclass-production.up.railway.app',
 });
 
 // Interceptor Request
